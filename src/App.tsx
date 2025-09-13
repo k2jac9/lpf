856ba31098e7704e6a2798cd8959042f59f7075f
Alejandro Castellanos <k2jac9@users.noreply.github.com>
2025-09-13 18:29:08 -0400

    Fix Petra wallet adapter package name to correct one

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { WalletProvider } from '@aptos-labs/wallet-adapter-react';
-import { PetraWallet } from '@aptos-labs/petra-plugin-wallet-adapter';
+import { PetraWallet } from '@aptos-labs/wallet-adapter-petra';
 import { MartianWallet } from '@aptos-labs/wallet-adapter-martian';
 import { PontemWallet } from '@aptos-labs/wallet-adapter-pontem';
 import { FewchaWallet } from '@fewcha/aptos-wallet-adapter';
